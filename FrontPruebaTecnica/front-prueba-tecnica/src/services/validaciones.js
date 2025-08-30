8f31b78c499eb78771d3261c660872edc1cbd64b
Braihan Steven Lopera Agudelo <braihanlopera@gmail.com>
2025-08-30 17:34:06 -0500

    Update validaciones.js

--- FrontPruebaTecnica/front-prueba-tecnica/src/services/validaciones.js
+++ FrontPruebaTecnica/front-prueba-tecnica/src/services/validaciones.js
@@ -1,10 +1,9 @@
-// correo válido: debe contener @ y al menos 4 letras después del @
+
 export function esUnCorreoValido(correo) {
   const regex = /^[^@]+@.{4,}$/;
   return regex.test(correo);
 }
 
-// (opcional) nombre válido: solo letras y espacios
 export function esUnNombreValido(nombre) {
   const regex = /^[A-Za-zÁÉÍÓÚáéíóúñÑ\s]+$/;
   return regex.test(nombre);
