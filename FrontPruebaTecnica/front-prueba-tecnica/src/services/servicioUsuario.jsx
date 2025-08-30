320ac67d02409bbf067f0a31bfcb77bba4849d94
Braihan Steven Lopera Agudelo <braihanlopera@gmail.com>
2025-08-30 17:34:44 -0500

    Update servicioUsuario.jsx

--- FrontPruebaTecnica/front-prueba-tecnica/src/services/servicioUsuario.jsx
+++ FrontPruebaTecnica/front-prueba-tecnica/src/services/servicioUsuario.jsx
@@ -1,28 +1,28 @@
 import React from "react";
 const API_URL = "http://localhost:8080";
 
-//traer los tipos de documentos
+
 export async function obtenerTiposDocumento() {
   const res = await fetch(`${API_URL}/tipo-identificacion/listar`);
   if (!res.ok) throw new Error("Error al cargar tipos de documento");
   return res.json();
 }
 
-//traer los paises
+
 export async function obtenerPaises() {
   const res = await fetch(`${API_URL}/paises/listar`);
   if (!res.ok) throw new Error("Error al cargar países");
   return res.json();
 }
 
-//traer los departamentios dependiendo del id del pais
+
 export async function obtenerDepartamentos(idPais) {
   const res = await fetch(`${API_URL}/departamento/listar/${idPais}`);
   if (!res.ok) throw new Error("Error al cargar departamentos");
   return res.json();
 }
 
-//traer las ciudades dependiendo del id del departamento
+
 export async function obtenerCiudades(idDepartamento) {
   const res = await fetch(`${API_URL}/ciudad/listar/${idDepartamento}`);
   if (!res.ok) throw new Error("Error al cargar ciudades");
@@ -30,7 +30,7 @@ export async function obtenerCiudades(idDepartamento) {
 }
 
 
-//(funcion de registrar)
+
 export async function registrarUsuario(usuario) {
   const res = await fetch(`${API_URL}/usuarios/guardar`, {
     method: "POST",
